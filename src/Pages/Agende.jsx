3f23f5b49761fd1db3f0c9562ebfa5295db54518
MARLON RAMOS DA COSTA <04389615017@senacrs.edu.br>
2025-06-27 10:06:55 -0300

    Ajuste Deploy

--- src/Pages/Agende.jsx
+++ src/Pages/Agende.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import iconeCoracao from '../assets/coracaobranco.png';
+import iconeCoracao from '../assets/coracaoBranco.png';
 import { Link } from 'react-router-dom'
 
 function Agende() {
