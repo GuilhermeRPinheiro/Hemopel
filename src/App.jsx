355bef06138e97c3c8aa377d2f2afeead82b4e5f
Marlon Ramos <marlonrdacosta@gmail.com>
2025-06-30 15:43:38 -0300

    Páginas João

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Routes, Route } from 'react-router-dom'
 import { MyNavbar } from './Components/Navbar'
 import { Footer } from './Components/Footer'
@@ -6,6 +5,7 @@ import Cadastro from './Pages/Cadastro'
 import Campanhas from './Pages/Campanhas'
 import Agende from './Pages/Agende'
 import Perfil from './Pages/Perfil'
+import Landing from './Pages/Landing'
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <MyNavbar />
       <main className="flex-grow ">
         <Routes>
+          <Route path="/" element={<Landing/>}/>
           <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/campanhas" element={<Campanhas />} />
           <Route path="/agende" element={<Agende />} />
